3ddac4bfbe2142d4c3d079f8943d80126127afa6
evborscheva <e.v.borscheva@gmail.com>
2025-12-02 22:21:40 +0300

    Изменен файл playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig (
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -34,6 +34,8 @@ export default defineConfig (
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     headless: process.env.CI ? true : false
    /* viewport: { width: 1280, height: 720 },
    screenshot, video: ''*/
